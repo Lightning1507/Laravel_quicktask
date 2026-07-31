897fb610f40a2b4b3e0ef2e4399a74716663d69d
Lightning <lightning1575@gmail.com>
2026-07-31 17:34:25 +0700

    Complete chapter 9 localization

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,45 +1,52 @@
 <x-guest-layout>
+    <div class="mb-4 flex items-center justify-end gap-2 text-sm">
+        <span class="text-gray-600 dark:text-gray-400">{{ __('messages.language') }}</span>
+        <a class="underline text-gray-600 dark:text-gray-400" href="{{ route('locale.update', 'en') }}">{{ __('messages.english') }}</a>
+        <span class="text-gray-400">|</span>
+        <a class="underline text-gray-600 dark:text-gray-400" href="{{ route('locale.update', 'vi') }}">{{ __('messages.vietnamese') }}</a>
+    </div>
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('messages.name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- First Name -->
         <div class="mt-4">
-            <x-input-label for="first_name" :value="__('First Name')" />
+            <x-input-label for="first_name" :value="__('messages.first_name')" />
             <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autocomplete="given-name" />
             <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
         </div>
 
         <!-- Last Name -->
         <div class="mt-4">
-            <x-input-label for="last_name" :value="__('Last Name')" />
+            <x-input-label for="last_name" :value="__('messages.last_name')" />
             <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autocomplete="family-name" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
         </div>
 
         <!-- Username -->
         <div class="mt-4">
-            <x-input-label for="username" :value="__('Username')" />
+            <x-input-label for="username" :value="__('messages.username')" />
             <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('messages.email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('messages.password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -51,7 +58,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('messages.confirm_password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -62,11 +69,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('messages.already_registered') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('messages.register') }}
             </x-primary-button>
         </div>
     </form>
